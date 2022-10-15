089abcffa6615812674004b87f2b33d2c5690477
Ahmedijaz11 <101427579+Ahmedijaz11@users.noreply.github.com>
2022-10-15 12:26:23 +0500

    Practice on diff operations

--- script.js
+++ script.js
@@ -32,3 +32,96 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+// ---------- CRUD OPERATIONS IN DOM --------
+
+
+//------------1 SELECTION---------
+
+
+
+//Different methods to select elements
+
+// 1--to select entire page css , or specfic area like head or body
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+
+//2 --selecting all sections (containing section class)
+const allSection = document.querySelectorAll('.section');
+
+// 3-- selecting by id  class name and tags 
+
+document.getElementById('section--1');
+
+const allButtons = document.getElementsByTagName('button');
+
+document.getElementsByClassName('btn');
+
+
+
+//------------2 CREATE AND INSERT---------
+
+
+
+//Creating and inserting elements
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+//message.textContent = 'we use cookies for improved functionality and analytics.';
+message.innerHTML = 'we use cookies for improved functionality and analytics <button class="btn btn--close-cookie"> Got it !</button>';
+
+//header.prepend(message);
+header.append(message);
+//header.before(message);
+//header.after(message);
+//header.append(message.cloneNode(true));
+
+
+
+//------------3 DELETE ELEMENTS---------
+
+document.querySelector('.btn--close-cookie').addEventListener('click', function(){
+  message.remove();
+});
+
+
+//----------END CRUD OPERATIONS------
+
+
+//-------NOW Working with STYLES---------
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+message.style.height = '58px'; 
+
+document.documentElement.style.setProperty('--color-primary', 'orange');
+
+
+//-------NOW Working with ATTRIBUTES---------
+
+const logo = document.querySelector('.nav__logo');
+logo.alt = " Beautiful minimalist logo";
+
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));// by using get attribute it will show absolute address
+
+//Data Attributes
+
+console.log(logo.dataset.versionNumber);
+
+//classes
+
+logo.classList.add('c', 'pqr');
+logo.classList.remove('c', 'lmn');
+logo.classList.toggle('abc');
+logo.classList.contains('crt');// not includes
+
+console.log(logo.classList);
+//Don't use
+logo.className = 'xyz';
